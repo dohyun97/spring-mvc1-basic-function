9d37df3eda04d4817f9f9f4a46180d929e227a34
dohyun97 <dohyun647@gmail.com>
2023-04-09 23:11:43 -0400

    Request mapping API

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,32 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/mapping/users")
+@RestController
+public class MappingClassController {
+    @GetMapping
+    public String users(){
+        return "get users";
+    }
+
+    @PostMapping
+    public String addUser(){
+        return "post user";
+    }
+
+    @GetMapping("/{userId}")
+    public String findUser(@PathVariable int userId){
+        return "get userId = "+userId;
+    }
+
+    @PatchMapping("/{userId")
+    public String updateUser(@PathVariable int userId){
+        return "update userId = "+userId;
+    }
+
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable int userId){
+        return "Delete userId = "+userId;
+    }
+}
